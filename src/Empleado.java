31e331ae7fe6b53d6d50a69e92822fba138a723d
lupixe <rodrigomacagno30@gmail.com>
2026-08-26 10:07:28 -0300

    2v

---
+++ src/Empleado.java
@@ -0,0 +1,25 @@
+public class Empleado {
+    private int dni;
+    private String Nombre;
+    private double salario;
+
+    public int getDni() {
+        return dni;
+    }
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+    public String getNombre(){
+        return getNombre();
+    }
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
